71b13c87357fadfa1bb9d3c9b3a675e252222f9d
David Sävenmark <dsinemark@gmail.com>
2021-02-03 21:36:02 +0100

    Initial Commit

---
+++ BandApp_MusicPlayer/BandApp_MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandApp_MusicPlayerApp.swift
+//  BandApp_MusicPlayer
+//
+//  Created by David on 2021-02-03.
+//
+
+import SwiftUI
+
+@main
+struct BandApp_MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BandApp_MusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BandApp_MusicPlayer
+//
+//  Created by David on 2021-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
